f5649a6025e71524227817ae886548da7bf82ea4
Jhonjandrys Ramirez <jhonjandrysramirez10@gmail.com>
2025-03-16 09:42:57 -0500

    fix: Update fetch CSS of responsive

--- file.js
+++ file.js
@@ -1,11 +1,14 @@
 const hamburguer = document.getElementById('hamburguer')
+const infoHome = document.getElementById('info-home')
 hamburguer.addEventListener('click', () => {
     const nav = document.getElementById('nav')
     nav.style.display = nav.style.display === 'none' ? 'block' : 'none'
-    nav.style.transition = 'all 0.5s'
+    nav.style.transition = 'all 0.6s ease-in-out'
+
+    if (window.innerWidth < 768) {
+        infoHome.style.display = infoHome.style.display === 'block' ? 'none' : 'block'
+    }
 })
-
-
 const data = [
     {
         imagen: '/images/cuadro1.png',
@@ -24,16 +27,34 @@ const data = [
 ]
 
 let div = document.createElement('div')
+let button = document.createElement('button')
+let containbutton = document.createElement('a')
+containbutton.appendChild(button)
+button.id = 'button-buy'
+containbutton.style.display = 'flex'
+containbutton.style.justifyContent = 'center'
+button.innerHTML = 'Ver todos los cuadros'
+let contain = document.getElementById('section-4')
 
 data.forEach((photos) => {
-    let contain = document.getElementById('section-4')
     let div1 = document.createElement('div') 
     // ESTILOS
     div.style.display = 'flex'
     div.style.justifyContent = 'center'
+    div.style.alignItems = 'center'
     div.style.gap = '15px'
     div1.style.display = 'flex'
     div1.style.flexDirection = 'column'
+    div1.style.alignItems = 'center'
+
+    if (window.innerWidth >= 768 && window.innerWidth <= 900) {
+        div.style.flexDirection = 'column'
+        div.style.alignItems = 'center'
+    }else if (window.innerWidth >= 1000 && window.innerWidth <= 1400) {
+        div.style.flexDirection = 'column'
+    }else{
+        div.style.flexDirection = 'row'
+    }
     // FINALIZA ESTILOS
     contain.appendChild(div)
     div.appendChild(div1)
@@ -43,3 +64,4 @@ data.forEach((photos) => {
     <p id='paragraph-photos'> <span>Desde:</span>  $${photos.precio}</p>
     `
 })
+contain.appendChild(containbutton)
